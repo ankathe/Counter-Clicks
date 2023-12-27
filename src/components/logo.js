0edbfd4d0b587d4c09a46fcdaf9240306dc5e3fe
katherine <angiecruzo.o@gmail.com>
2023-12-27 18:35:22 -0500

    add link linkedin

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,13 +1,15 @@
-import  React from 'react';
-import logo from '../images/logo-projects-react.png';
-import './logo.css';
+import React from "react";
+import logo from "../images/logo-projects-react.png";
+import "./logo.css";
 
-function Logo(){
-    return (
-        <div className="logo-container">
-            <img className="logo" src={logo} alt="logo clicks counter" />
-        </div>
-    )
-};
+function Logo() {
+  return (
+    <div className="logo-container">
+      <a href="https://www.linkedin.com/in/katherine-cruz-7588b763" className="link">
+        <img className="logo" src={logo} alt="logo clicks counter" />
+      </a>
+    </div>
+  );
+}
 
 export default Logo;
